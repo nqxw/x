04896698e47957fea390cad2d888e5d6e2cc6b20
oli <nothinggg156@gmail.com>
2026-09-20 06:04:49 -0400

    Replace discord import with modifyself_compat

--- selfbot.py
+++ selfbot.py
@@ -1,8 +1,7 @@
 # selfbot.py | Python 3.10+ | discord.py-self + aiohttp
 # sy's selfbot — v2.2.6
 
-import modifyself
-import discord
+import modifyself_compat as discord
 import asyncio
 import aiohttp
 import json
